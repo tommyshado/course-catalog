b2493b100dfabf1589a6606a8b7eb86600ead87f
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-20 15:19:09 +0200

    added test cases for when the when the starts between 10 and 15 of march

--- course-cost/test/cost.test.js
+++ course-cost/test/cost.test.js
@@ -63,7 +63,57 @@ describe('courseCost function', () => {
                                 }
             assert.deepEqual(returnedValue, expectedValue);
         })
+        it('when the user starts on the 10th and selects `r1` they will get 20% discount', () => {
+            let returnedValue = courseCost('r1', '2023-05-10');
+            let expectedValue = {
+                                    status : "success",
+                                    code: "r1",
+                                    description : "Ruby on Rails intro",
+                                    cost : 5675,
+                                    discount: 1135,
+                                    amountDue : 4540
+                                }
+            assert.deepEqual(returnedValue, expectedValue)
+        });
 
-    
+        it('when the user starts on the 10th and selects `p2` they will get 20% discount', () => {
+            let returnedValue = courseCost('p2', '2023-05-10');
+            let expectedValue = {
+                                    status : "success",
+                                    code: "r1",
+                                    description : "Intro to PHP course",
+                                    cost : 8990,
+                                    discount: 1798,
+                                    amountDue : 7192
+                                }
+            assert.deepEqual(returnedValue, expectedValue)
+        });
+
+        it('when the user starts on the 15th and selects `p1 they will get 20% discount`', () => {
+            let returnedValue = courseCost('p1', '2023-05-10');
+            let expectedValue = {
+                                    status : "success",
+                                    code: "r1",
+                                    description : "Python",
+                                    cost : 7850,
+                                    discount: 1570,
+                                    amountDue : 6280
+                                }
+                                console.log(returnedValue);
+            assert.deepEqual(returnedValue, expectedValue)
+        });
+
+        it('when the user starts on the 15th and selects `p2` they will get 20% discount', () => {
+            let returnedValue = courseCost('p2', '2023-05-10');
+            let expectedValue = {
+                                    status : "success",
+                                    code: "r1",
+                                    description : "Intro to PHP course",
+                                    cost : 8990,
+                                    discount: 1798,
+                                    amountDue : 7192
+                                }
+            assert.deepEqual(returnedValue, expectedValue)
+        });
     });
 })
