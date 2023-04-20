3450578999a051c629b9e0a6cff2151d7f1b8c79
Andre Vermeulen <andre.vermeulen@gmail.com>
2023-04-20 08:07:45 +0200

    Update courseCost.js

--- course-cost/courseCost.js
+++ course-cost/courseCost.js
@@ -1,18 +1,19 @@
 function courseCost(courseCode, startDateStr){
-    const startDate = new Date(startDateStr);
+
+    const startDate = new Date(startDateStr);  
 
     const courses = {
         "p1" : {
             courseName : "Python",
-            cost : "R7850.00"
+            cost : 7850.00
         },
         "p2" : {
             courseName : "Intro to PHP course",
-            cost : "R8990.00"
+            cost : 8990.00
         }, 
         "r1" : {
             courseName : "Ruby on Rails intro",
-            cost : "R5675.00"
+            cost : 5675.00
         }
     }
 
@@ -23,23 +24,23 @@ function courseCost(courseCode, startDateStr){
         }
     }
 
-    const day = startDate.getDay();
-
+    const day = Number(startDate.getDate());
+  
     let discountRate = 0;
     let courseCost = course.cost;
     let discount = 0;
 
     if ( day >=20 && day <=25) {
-        discountRate = 0.4
+        discountRate = 0.4      
     } else if ( day >=10 && day <=15) {
         discountRate = 0.2;
     }
 
     if (discountRate > 0) {
-        courseCost = course.cost * (1-discountRate)
         discount = courseCost * discountRate;
+      	courseCost = course.cost - discount;
     }
-
+  
     return {
         status : "success",
         code: "r1",
