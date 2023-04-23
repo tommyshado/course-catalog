4be087664107c4f0030ab67ae0db03688ab969d1
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-23 10:51:31 +0200

    testing the overTimeHoursObj in line 17

--- tutor-wages/test/tutorOvertimeWageCalculator.js
+++ tutor-wages/test/tutorOvertimeWageCalculator.js
@@ -14,6 +14,7 @@ function tutorOvertimeWageCalculator(timeSheet, tutorLvel) {
             overTimeHoursObj.levelThree += currentHours;
         }
     }
+    console.log(overTimeHoursObj)
 }
 
 let returnedValue = tutorOvertimeWageCalculator("8-8-8-8-8-9-5", 1);
