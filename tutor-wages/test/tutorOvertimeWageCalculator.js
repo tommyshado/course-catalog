02d36f8598b21f819bc41b5e9f0a3178832d7e71
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-04-23 11:20:43 +0200

    added two more conditional statements

--- tutor-wages/test/tutorOvertimeWageCalculator.js
+++ tutor-wages/test/tutorOvertimeWageCalculator.js
@@ -1,5 +1,9 @@
 function tutorOvertimeWageCalculator(timeSheet, tutorLvel) {
-    const overTimeHoursObj = { 'levelOne' : 0, 'levelTwo' : 0, 'levelThree' : 0 }
+    const overTimeHoursObj = { 
+        'levelOne' : 0,
+        'levelTwo' : 0, 
+        'levelThree' : 0 
+    }
 
     let splitTimeSheet = timeSheet.split('-');
     // iterate starting from second last element
@@ -18,12 +22,18 @@ function tutorOvertimeWageCalculator(timeSheet, tutorLvel) {
     }
     // iterate over the length of the object
     for (let currentLevel in overTimeHoursObj) {
-        if (overTimeHoursObj[currentLevel] > 0) {
+        if (overTimeHoursObj[currentLevel] > 0 && currentLevel === 'levelOne') {
             // hours per week supposed to be 40
             return ((overTimeHoursObj[currentLevel] - 40) * (75)) * 7 / 100 + (overTimeHoursObj[currentLevel] - 40) * (75);
+        } else if (overTimeHoursObj[currentLevel] > 0 && currentLevel === 'levelTwo') {
+            // hours per week supposed to be 40
+            return ((overTimeHoursObj[currentLevel] - 40) * (90)) * 9 / 100 + (overTimeHoursObj[currentLevel] - 40) * (90);
+        } else if (overTimeHoursObj[currentLevel] > 0 && currentLevel === 'levelOne') {
+            // hours per week supposed to be 40
+            return ((overTimeHoursObj[currentLevel] - 40) * (105)) * 12 / 100 + (overTimeHoursObj[currentLevel] - 40) * (105);
         }
     }
 }
 
-let returnedValue = tutorOvertimeWageCalculator("8-8-8-8-8-9-5", 1);
+let returnedValue = tutorOvertimeWageCalculator("8-8-8-10-8-8-7", 1);
 console.log(returnedValue);
